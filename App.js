ce187f4608dcc0f2822eff5f52507c790c110cbc
kridiramilli <kridiramilli@gmail.com>
2022-08-22 13:16:16 +0200

    FINISH: Navigation functionality Tab and Stack

--- App.js
+++ App.js
@@ -1,12 +1,57 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Prayer, Dhikr, Qibla } from "./screens";
+const TabNavigator = createBottomTabNavigator();
 export default function App() {
+  const screenOptions = {
+    unmountOnBlur: false,
+    tabBarStyle: {
+      backgroundColor: "#fff",
+      width: "85%",
+      height: 60,
+      border: "1px solid #eee",
+      borderRadius: 30,
+      marginBottom: 20,
+      position: "absolute",
+      left: "50%",
+      shadowColor: "#ddd",
+      transform: [{ translateX: "-50%" }],
+      shadowOffset: {
+        width: 0,
+        height: 0,
+      },
+      shadowOpacity: 0.2,
+      shadowRadius: 10.84,
+      elevation: 5,
+    },
+    tabBarItemStyle: {
+      // backgroundColor: "#00ff00",
+      margin: 5,
+      borderRadius: 30,
+    },
+  };
+  const sceneContainerStyle = {
+    backgroundColor: "teal",
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style={{ backgroundColor: "red" }} />
-    </View>
+    <NavigationContainer>
+      <TabNavigator.Navigator
+        initialRouteName='PrayerTimes'
+        {...{ screenOptions, sceneContainerStyle }}
+      >
+        <TabNavigator.Screen name='Dhikr' component={Dhikr} />
+        <TabNavigator.Screen
+          name='Prayer Times'
+          component={Prayer}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <TabNavigator.Screen name='Qibla Finder' component={Qibla} />
+      </TabNavigator.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/index.js
@@ -0,0 +1,8 @@
+import PrayerTimes from "./PrayerTimes";
+import Schedule from "./Schedule";
+import Qibla from "./Qibla";
+import Settings from "./Settings";
+import Dhikr from "./Dhikr";
+import Prayer from "./Prayer";
+
+export { PrayerTimes, Schedule, Settings, Dhikr, Qibla, Prayer };
