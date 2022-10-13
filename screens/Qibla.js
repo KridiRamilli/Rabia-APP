2db980b5292e479dff061d65b6e04f8365ef30a2
kridiramilli <kridiramilli@gmail.com>
2022-10-13 23:30:18 +0200

    FEATURE: Add arrow guidance of finding Qibla direction

--- screens/Qibla.js
+++ screens/Qibla.js
@@ -7,6 +7,7 @@ import Animated, {
 	withTiming,
 	useAnimatedStyle,
 	Easing,
+	withRepeat,
 } from "react-native-reanimated";
 
 import {
@@ -16,17 +17,15 @@ import {
 	approxValue,
 } from "./../utils";
 
-import { IMAGES } from "../constants";
+import { IMAGES, ICONS } from "../constants";
 import { COLORS, SIZES, FONTS } from "../theme/theme";
 
 export const Qibla = () => {
-	const [locationCoords, setLocationCoords] = useState({
-		latitude: 0,
-		longitude: 0,
-	});
 	const [qiblaAngle, setQiblaAngle] = useState(0);
 	const [directionAngle, setDirectionAngle] = useState(90);
 	const [isFound, setIsFound] = useState(false);
+	const [showLeftIcon, setShowLeftIcon] = useState(false);
+	const [showRightIcon, setShowRightIcon] = useState(false);
 
 	useEffect(() => {
 		(async () => {
@@ -40,24 +39,46 @@ export const Qibla = () => {
 				magHeading = Math.round(magHeading);
 				setDirectionAngle(magHeading);
 			});
+
+			//Called once so it does repeat as shown
+			opacity.value = withRepeat(
+				withTiming(1, { duration: 1000, easing: Easing.ease }),
+				0,
+				true
+			);
 		})();
 	}, []);
+
 	useEffect(() => {
 		if (approxValue(directionAngle, qiblaAngle, 10)) {
 			setIsFound(true);
 		} else {
 			setIsFound(false);
 		}
+		if (directionAngle > qiblaAngle && !isFound) {
+			setShowLeftIcon(true);
+			setShowRightIcon(false);
+		} else if (directionAngle < qiblaAngle && !isFound) {
+			setShowRightIcon(true);
+			setShowLeftIcon(false);
+		} else {
+			setShowLeftIcon(false);
+			setShowRightIcon(false);
+		}
 		spinValue.value = withTiming(`${-directionAngle}deg`, config);
 	}, [directionAngle]);
+
 	useEffect(() => {
 		if (isFound === true) {
 			Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
 		}
 	}, [isFound]);
+
 	const spinValue = useSharedValue(0);
+	const opacity = useSharedValue(0);
+
 	const config = {
-		duration: 200,
+		duration: 150,
 		easing: Easing.linear,
 	};
 
@@ -70,11 +91,31 @@ export const Qibla = () => {
 			],
 		};
 	}, [directionAngle]);
+	const arrowStyle = useAnimatedStyle(() => ({ opacity: opacity.value }), []);
 
 	return (
 		<SafeAreaView style={[styles.container, isFound ? styles.found : ""]}>
-			<Text style={[styles.title, FONTS.h1]}>Kibla</Text>
+			<Text style={[styles.title]}>Qibla</Text>
 			<View style={styles.imageContainer}>
+				<Animated.View style={[styles.arrowsContainer, arrowStyle]}>
+					{showLeftIcon ? (
+						<Image
+							source={ICONS.qibla_left_arrow_icon}
+							style={styles.arrowIcon}
+						/>
+					) : (
+						<View></View>
+					)}
+
+					{showRightIcon ? (
+						<Image
+							source={ICONS.qibla_right_arrow_icon}
+							style={styles.arrowIcon}
+						/>
+					) : (
+						<View></View>
+					)}
+				</Animated.View>
 				<Text style={styles.text}>{directionAngle}</Text>
 				<Animated.Image
 					style={[styles.image, animatedStyle]}
@@ -106,19 +147,27 @@ const styles = StyleSheet.create({
 		backgroundColor: COLORS.black,
 	},
 	found: {
-		backgroundColor: COLORS.primary,
+		backgroundColor: "#55efc4",
 	},
 	title: {
+		...FONTS.h1,
 		color: COLORS.white,
 	},
 	imageContainer: {
 		justifyContent: "center",
 		alignItems: "center",
 		flex: 1,
+		width: "90%",
+	},
+	arrowsContainer: {
+		width: "90%",
+		flex: 0.3,
+		flexDirection: "row",
+		justifyContent: "space-between",
 	},
 	image: {
 		resizeMode: "contain",
-		width: SIZES.width * 0.7,
+		width: SIZES.width * 0.65,
 		height: SIZES.height * 0.4,
 	},
 	footer: {
@@ -137,5 +186,12 @@ const styles = StyleSheet.create({
 		fontFamily: "Roboto-Regular",
 		fontSize: SIZES.h2,
 		padding: 1,
+	},
+	arrowIcon: {
+		width: 45,
+		height: 45,
+		resizeMode: "contain",
+		justifySelf: "flex-start",
+		tintColor: "red",
 	},
 });
