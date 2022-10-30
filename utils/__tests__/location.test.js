d4019dea9dac6f3c9820a1b984cd4410cbbf2ea5
kridiramilli <kridiramilli@gmail.com>
2022-10-30 22:30:37 +0100

    ADD: Location utils tests and imports

---
+++ utils/__tests__/location.test.js
@@ -0,0 +1,9 @@
+const {
+	requestLocationPermission,
+	getLocationPermission,
+	getLocationAddress,
+	getLocationHeading,
+	degree,
+	calculateQiblaAngle,
+	getLocationCoords,
+} = require("./location");
